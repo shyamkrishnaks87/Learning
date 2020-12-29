5760b67bc2f62226aea4ccb28d922af93a213dd2
Shyam Krishna <51166642+shyamkrishnaks87@users.noreply.github.com>
2020-12-29 09:58:00 +0530

    Create a basic GLFW window

---
+++ glfw_window.cpp
@@ -0,0 +1,37 @@
+#include<GL/glew.h>
+#include<GLFW/glfw3.h>
+#include<iostream>
+
+static void errorGLFW(int error, const char* description) {
+	std::cout << "Error : " << description << std::endl;
+}
+
+void startWindow(std::string& title)
+{
+	glfwSetErrorCallback(errorGLFW);
+	if (!glfwInit())
+	{
+		std::cout << "Failed to initialize GLFW." << std::endl;
+		return;
+	}
+
+	GLFWwindow* window = glfwCreateWindow(640, 480, title.c_str(), nullptr, nullptr);
+	if (!window) {
+		std::cout << "Failed to create window and opengl context" << std::endl;
+		return;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	while (!glfwWindowShouldClose(window)) 
+	{
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+}
+
+int main() {
+	std::string title = "My sample GLFW Window";
+	startWindow(title);
+}
